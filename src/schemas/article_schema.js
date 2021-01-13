6b8b30c0052c02938414870cc4b4286f6b5404c0
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-01-13 14:23:28 +0100

    added reviews array

--- src/schemas/article_schema.js
+++ src/schemas/article_schema.js
@@ -1,16 +1,22 @@
 const { Schema, model } = require("mongoose")
 
 const ArticleSchema = new Schema({
-    "headLine": String,
-    "subHead": String,
-    "content": String,
-    "category": String,
-    "author": {
-        "name": String,
-        "img": String
+    headLine: String,
+    subHead: String,
+    content: String,
+    category: String,
+    author: {
+        name: String,
+        img: String
     },
-    "cover": String,
+    cover: String,
 },
+    {
+        reviews: [{
+            text: String,
+            user: String
+        }]
+    },
     {
         timestamps: true
     }
